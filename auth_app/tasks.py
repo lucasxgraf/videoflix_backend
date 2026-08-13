18d7a4fd61bd754ac322eed5f0a9d9ae76ef128c
Lucas Graf <lucasgraf01@gmail.com>
2026-08-13 21:29:06 +0200

    fix: Embed email logo inline and correct password reset validity hours
    Gmail fetches remote email images server-side and can never reach a
    localhost backend URL, so the logo showed as broken. Embed it as a
    CID inline attachment instead of linking to BACKEND_URL/static/.
    
    Also PASSWORD_RESET_TIMEOUT is in seconds, but was passed straight
    into the template as if it were hours (showing "86400 hours").

--- auth_app/tasks.py
+++ auth_app/tasks.py
@@ -1,7 +1,12 @@
+from email.mime.image import MIMEImage
+
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
+
+LOGO_CID = 'logo'
+LOGO_PATH = settings.BASE_DIR / 'auth_app' / 'static' / 'auth_app' / 'images' / 'logo.png'
 
 
 def send_activation_email(user_id, uidb64, token):
@@ -13,7 +18,6 @@ def send_activation_email(user_id, uidb64, token):
 
     username = user.email.split('@')[0]
     activation_link = f'{settings.FRONTEND_URL}/activate/{uidb64}/{token}/'
-    backend_url = settings.BACKEND_URL
 
     subject = 'Videoflix - Activate Your Account'
     plain_text_message = (
@@ -21,11 +25,11 @@ def send_activation_email(user_id, uidb64, token):
         f'Please activate your account by clicking the link below:\n\n'
         f'{activation_link}\n\nThank you!'
     )
-    
+
     _send_html_email(user, subject, plain_text_message, 'email/activation_email.html', context={
         'username': username,
         'activation_link': activation_link,
-        'backend_url': backend_url,
+        'logo_cid': LOGO_CID,
     })
 
 
@@ -38,8 +42,7 @@ def send_password_reset_email(user_id, uidb64, token):
 
     username = user.email.split('@')[0]
     password_reset_link = f'{settings.FRONTEND_URL}/reset-password/{uidb64}/{token}/'
-    backend_url = settings.BACKEND_URL
-    password_reset_link_valid_hours = settings.PASSWORD_RESET_TIMEOUT
+    password_reset_link_valid_hours = settings.PASSWORD_RESET_TIMEOUT // 3600
 
     subject = 'Videoflix - Reset Your Password'
     plain_text_message = (
@@ -47,12 +50,12 @@ def send_password_reset_email(user_id, uidb64, token):
         f'Please reset your password by clicking the link below:\n\n'
         f'{password_reset_link}\n\nThank you!'
     )
-    
+
     _send_html_email(user, subject, plain_text_message, 'email/password_reset_email.html', context={
         'username': username,
         'password_reset_link': password_reset_link,
-        'backend_url': backend_url,
         'password_reset_link_valid_hours': password_reset_link_valid_hours,
+        'logo_cid': LOGO_CID,
     })
 
 
@@ -64,4 +67,11 @@ def _send_html_email(user, subject, plain_text_message, template_name, context):
     html_message = render_to_string(template_name, context)
     email = EmailMultiAlternatives(subject, plain_text_message, settings.DEFAULT_FROM_EMAIL, [user.email])
     email.attach_alternative(html_message, "text/html")
+
+    with open(LOGO_PATH, 'rb') as logo_file:
+        logo_image = MIMEImage(logo_file.read())
+    logo_image.add_header('Content-ID', f'<{LOGO_CID}>')
+    logo_image.add_header('Content-Disposition', 'inline', filename='logo.png')
+    email.attach(logo_image)
+
     email.send()
